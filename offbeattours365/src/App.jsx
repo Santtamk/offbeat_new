8e5073cd035f2b3604df3a781b803784a4559b28
Santam Kumai <santtamk@gmail.com>
2023-10-17 20:45:03 +0530

    first commit

---
+++ offbeattours365/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './component/Navbar/Navbar'
+import Header from './component/Header/Header'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <Navbar/>
+      {/* <Hero/> */}
+    </>
+  )
+}
+
+export default App
---
+++ offbeattours365/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div>Home</div>
+        <div>About Us</div>
+        <div>Destinations</div>
+        <div>Packages</div>
+        <div>Contact Us</div>
+    </div>
+  )
+}
+
+export default Navbar;
